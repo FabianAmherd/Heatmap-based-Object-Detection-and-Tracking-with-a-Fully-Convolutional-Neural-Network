e87c9270e3b6e97cd61a6fed79c955e77b99d3b7
Elias Rodriguez <elias.rodriguez.privat@gmail.com>
2020-04-17 10:48:39 +0200

    Revert "preprocessing dot py fixed"
    This reverts commit 86806ecb957cf61a2d728f54225c5b2e9343a60f.

--- python-scripts/preprocessing.py
+++ python-scripts/preprocessing.py
@@ -2,9 +2,9 @@
 #
 #
 #Change environment with: conda activate pt-labi
-# import av
+import av
 import numpy as np
-# from PIL import Image
+from PIL import Image
 from numpy import save
 #from numpy import load # Use this to load the .npy arrays later
 import math
@@ -44,44 +44,32 @@ for frame in container.decode(video=0):
 
 
 # read out locations.txt file, turn into numpy array and apply 2D Gaussian distribution
-import numpy as np
-import os
-import sys
-import pandas as pd
-from numpy import save
-from numpy import load
-
-np.set_printoptions(threshold=sys.maxsize)
-
-
 array_frames = []
 count = 0
 
-with open('./test.txt') as fp:
-   line = fp.readline()
-   while line:
-      wholeLine = line.strip()
-      if ('#' != wholeLine[0]):
-         frame = np.zeros((100,100))
-         data = wholeLine.split()
-         x_converted = round((int(data[3]) / 240) * 100)
-         y_converted = round((int(data[4]) / 180) * 100)
-         if data[3] != "-1" or data[4] != "-1":
+with open('0Preprocessing_data/jAER_100^2_all_imgs_betterlables/jAER_simplest_by_hand_better-targetLocations.txt') as fp:
+    # Get the first line
+    line = fp.readline()
+    while line:
+        wholeLine = line.strip()
+        if ('#' != wholeLine[0]) and ('A' != wholeLine[0]):
+            frame = np.zeros((100, 100))
+            data = wholeLine.split()
+            x_converted = round((int(data[3]) / 240) * 100)
+            y_converted = round((int(data[4]) / 180) * 100)
+
             if (x_converted >= 0) and (y_converted >= 0):
-               for y in range(len(frame)):
-                  y_array = frame[y]
-                  for x in range(len(y_array)):
-                     x_d = x - x_converted 
-                     y_d = y - y_converted 
-                     
-                     distance = math.sqrt(x_d ** 2.0 + y_d ** 2.0) 
-                     frame[x][y] = math.exp( - ((distance) ** 2.0))
-   
-            save(f'./output/label-{count}.npy',frame)
+                frame[x_converted][y_converted] = 1.0
+                for y in range(len(frame)):
+                    y_array = frame[y]
+                    for x in range(len(y_array)):
+                        x_d = x - x_converted
+                        y_d = y - y_converted
+
+                        distance = math.sqrt(x_d ** 2.0 + y_d ** 2.0)
+                        frame[x][y] = math.exp(- (distance ** 2.0))
+
+            save(f'0Preprocessing_data/jAER_100^2_all_imgs_betterlables/labels/label-{count}.npy', frame)
             count += 1
 
-
-      line = fp.readline()
-
-
-converted_frame = np.around(frame, decimals = 2)
+        line = fp.readline()
