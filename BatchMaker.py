a432c54a7eb3756dc5d21f9993bad64c45927ae7
Fabian Amherd <fabian.amherd@gmail.com>
2020-05-22 16:00:40 +0200

    Prepared for test set

--- BatchMaker.py
+++ BatchMaker.py
@@ -75,8 +75,6 @@ def BatchMaker(images_path):
          path, path1, path2, label = zipped.__next__()
          Img = Labels(label)
          
-   
-
          line = fp.readline()
          wholeLine = line.strip()
          data = wholeLine.split()
